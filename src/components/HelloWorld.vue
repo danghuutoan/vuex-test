7a1a79671544064d98453e6fc681c5d96f391baa
danghuutoan <danghuutoan2002@gmail.com>
2017-10-09 14:36:31 +0700

    added mutations and actions example, imported element-ui

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,17 +1,29 @@
 <template>
-	<div class="hello">
-		Counter: {{ $store.state.count }}, counter is {{ evenOrOdd }}
+	<div class="container">
+		<div class="row text-center">
+			<h3>Clicked {{ $store.state.count }} times , counter is {{ evenOrOdd }} </h3>
+			<el-button type="success" @click="increment" >+</el-button>
+    		<el-button type="danger" @click="decrement" >-</el-button>
+    		<el-button @click="incrementIfOdd" >Increment if odd</el-button>
+    		<el-button @click="incrementAsync" >Increment async</el-button>
+		</div>
 	</div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters, mapActions } from 'vuex'
 
 export default {
 	name: 'HelloWorld',
 	computed: mapGetters([
 		'evenOrOdd'
 	]),
+	methods: mapActions([
+		'increment',
+		'decrement',
+		'incrementIfOdd',
+		'incrementAsync'
+	])
 }
 </script>
 
